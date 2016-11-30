c23f927697c8d4a87226e0b99cfbd04e216c9cf3
Eric Shans <ericshans@erics-imac.fios-router.home>
2016-11-29 21:52:23 -0500

    what the hell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   declarations: [
     AppComponent
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
