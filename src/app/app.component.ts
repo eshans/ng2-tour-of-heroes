3d19b7bf11a16ef60296b536b1c6a35065a9d6fd
Greg Deane <greg.james.deane@zalando.de>
2017-01-23 09:44:33 +0100

    fix(app): remove duplicated component declarations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,6 @@
-
 import { Component, OnInit } from '@angular/core';
-
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
-
-@Component({
-  selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="let hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-  `,
-  
-import { Component } from '@angular/core';
-import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
